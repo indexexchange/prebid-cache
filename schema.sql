dd185b36bf62c98dcd2f183143504050c2689c81
Mike Burns <mike.burns@indexexchange.com>
2021-12-07 16:34:12 -0500

    Migrate ci/cd from common

--- schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE KEYSPACE prebid WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'};
-
-CREATE TABLE prebid.cache (
-    key text,
-    value text,
-    PRIMARY KEY (key)
-);
